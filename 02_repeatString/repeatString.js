db36439eb9b508f94e720cfebfbda2de2b70744d
Dothat65 <meemoahmed2004@gmail.com>
2024-10-15 15:31:07 -0400

    adding functionality to functions to test them using jest.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    // repeat string N number of times
+    // two parameters, string, and number 
+    // i'll loop the amount of the number, and append it to a result string
+    let result = ""
+    if(number <0){
+        return "ERROR";
+    }
+    for(let i=0; i<number;i++){
+        result+= string;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //taken a string in input
+    // make a new string, reverse each character starting at end
+    // return that new string
+    let result  = ""
+    for(let i=string.length-1; i>=0; i--){
+        result += string.charAt(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
